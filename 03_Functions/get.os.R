8db8551a4032a2266095a76c6ce2a59641f63f0a
biodray <audrey.bourret@usherbrooke.ca>
2018-11-29 15:45:16 -0500

    add os function

---
+++ 03_Functions/get.os.R
@@ -0,0 +1,19 @@
+# Identifying os from r
+
+# http://conjugateprior.org/2015/06/identifying-the-os-from-r/
+
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }  
+  tolower(os)
+}
