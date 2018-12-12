26819d1eee4893e95f01cd9bc17a5aae127b0425
biodray <audrey.bourret@usherbrooke.ca>
2018-12-12 09:27:24 -0500

    Update file structure

--- 02_Codes/Change_PARAM.R
+++ 02_Codes/Change_PARAM.R
@@ -8,8 +8,10 @@
 
 # Library -----------------------------------------------------------------
 
-#  
-source(file.path("./03_Functions",  list.files("./03_Functions")))
+# Internal functions
+for(i in 1:length( list.files("./03_Functions") )){
+  source(file.path("./03_Functions",  list.files("./03_Functions")[i]))  
+}
 
 
 PARAM <- data.frame(x = character(), value = character())
@@ -36,18 +38,28 @@ add.param(PARAM) <- c("raw.path", "./00_Data/01a_RawData")
 add.param(PARAM) <- c("raw_unz.path", "./00_Data/01b_RawData_unzipped")
 add.param(PARAM) <- c("raw_unz_rename.path", "./00_Data/01c_RawData_unzipped_rename")
 
-add.param(PARAM) <- c("filt_dada2.path", "./00_Data/02a_Filtered_dada2")
+add.param(PARAM) <- c("filt_cutadapt.path", "./00_Data/02a_Cutadapt")
+add.param(PARAM) <- c("filt_dada2.path", "./00_Data/02b_Filtered_dada2")
 
-add.param(PARAM) <- c("filt_usearch.merge.path", "./00_Data/03a_Merged_usearch")
-add.param(PARAM) <- c("filt_cutadapt.path", "./00_Data/03b_Cutadap")
+add.param(PARAM) <- c("ASV.dada2.path", "./00_Data/03_ASV")
+
+add.param(PARAM) <- c("filt_merge.path", "./00_Data/04a_Merged_usearch")
+add.param(PARAM) <- c("filt_derep.path", "./00_Data/04b_Derep_vsearch")
+add.param(PARAM) <- c("filt_derep.1file.path", "./00_Data/04c_Derep_united_vsearch")
+add.param(PARAM) <- c("OTU.usearch", "./00_Data/04d_OTU")
+add.param(PARAM) <- c("Compare.OTU.usearch", "./00_Data/04e_Compared_OTU")
+
+#add.param(PARAM) <- c("filt_maxEE.usearch.path", "./00_Data/04b_maxEE_usearch")
+
                       
 #add.param(PARAM) <- c("filt_JAMP.path", "./00_Data/02c_Filtered_JAMP")
 
-add.param(PARAM) <- c("ref.path", "./00_Data/04_RefSeq")
+add.param(PARAM) <- c("ref.path", "./00_Data/05_RefSeq")
 
 
 
 # Results
+
 add.param(PARAM) <- c("result.path", "./01_Results")
 add.param(PARAM) <- c("result.data.path", "./01_Results/01_data")
 add.param(PARAM) <- c("result.Q.path", "./01_Results/02_ReadsQ")
